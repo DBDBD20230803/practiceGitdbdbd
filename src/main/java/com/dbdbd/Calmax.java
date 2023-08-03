af5e9d4ffa2e24399e1076d58aca34a580ce79a2
ksostuft <ksostuft@gmail.com>
2023-08-03 19:44:18 +0900

    Calmax, Calmod 클래스 만들고, calmax(), calmod() 메소드 만들었습니다.

---
+++ src/main/java/com/dbdbd/Calmax.java
@@ -0,0 +1,14 @@
+package com.dbdbd;
+
+import java.util.Scanner;
+
+public class Calmax {
+    public static void calMax() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int a = Integer.parseInt(scanner.nextLine());
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int b = Integer.parseInt(scanner.nextLine());
+        System.out.println(Math.max(a, b));
+    }
+}
