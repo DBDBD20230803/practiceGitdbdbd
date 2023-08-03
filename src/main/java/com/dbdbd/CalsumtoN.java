6b92f30ac05d8b076632b1dbd90591e25a219141
lSuns <j76067469@gmail.com>
2023-08-03 20:04:07 +0900

    lSuns - update3

---
+++ src/main/java/com/dbdbd/CalsumtoN.java
@@ -0,0 +1,17 @@
+package com.dbdbd;
+
+import java.util.Scanner;
+
+public class CalsumtoN {
+    public static void calSumtoN() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1 부터 n 까지의 정수 값을 합을구합니다.");
+        System.out.print("n 의 값을 입력하세요");
+        int num = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+        }
+        System.out.println("1부터 n까지 합의 값은 " + sum);
+    }
+}
